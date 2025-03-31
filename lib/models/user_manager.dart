4a846dc8deec05ff2275e6fff6dd9d99be993c54
22moschen <murilomoschen327@gmail.com>
2025-03-31 16:15:32 -0300

    carregando usuários ao abrir o app

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,42 +1,51 @@
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
 import 'package:loja_virtual_pro/helpers/firebase_errors.dart';
-//import 'package:flutter/services.dart';
 import 'package:loja_virtual_pro/models/user.dart' as app_user;
 
 class UserManager extends ChangeNotifier {
-  final firebase_auth.FirebaseAuth auth = firebase_auth.FirebaseAuth.instance;
-
-  bool loading = false;
-
-  Future<void> signIn({
-    required app_user.User user, // Marcar como required
-    required Function(String)
-    onFail, // Marcar como required e especificar tipo do parâmetro (String)
-    required Function(firebase_auth.User)
-    onSuccess, // Marcar como required e especificar tipo do parâmetro (firebase_auth.User)
-  }) async {
-    setLoading(true);
-    try {
-      final firebase_auth.UserCredential result = await auth
-          .signInWithEmailAndPassword(
-            email: user.email,
-            password: user.password,
-          );
-    } on firebase_auth.FirebaseAuthException catch (e) {
-      // Exibe a mensagem de erro recebida do Firebase.
-      onFail(getErrorString(e.code));
-    }
-    setLoading(false);
+  UserManager() {
+    _loadCurrentUser();
   }
 
-  void setLoading(bool value) {
-    loading = value;
+  final firebase_auth.FirebaseAuth auth = firebase_auth.FirebaseAuth.instance;
+  firebase_auth.User? user;
+
+  bool _loading = false;
+  bool get loading => _loading;
+
+  Future<void> signIn({
+    required app_user.User user,
+    required Function(String) onFail,
+    required Function(firebase_auth.User) onSuccess,
+  }) async {
+    loading = true;
+    try {
+      final firebase_auth.UserCredential result =
+          await auth.signInWithEmailAndPassword(
+        email: user.email,
+        password: user.password,
+      );
+
+      this.user = result.user;
+      onSuccess(this.user!);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      onFail(getErrorString(e.code));
+    }
+    loading = false;
+  }
+
+  set loading(bool value) {
+    _loading = value;
     notifyListeners();
   }
 
-  @override
-  void notifyListeners() {
-    super.notifyListeners(); // Notificar os listeners
+  Future<void> _loadCurrentUser() async {
+    final firebase_auth.User? currentUser = auth.currentUser;
+    if (currentUser != null) {
+      user = currentUser;
+      print(user!.uid);
+    }
+    notifyListeners();
   }
 }
